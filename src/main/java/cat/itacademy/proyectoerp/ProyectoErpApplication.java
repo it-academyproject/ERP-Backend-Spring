cebae62501a15a6738f7a64427ae013f2e081d8d
nevure <nevure@gmail.com>
2021-01-18 12:45:30 +0100

    primer commit

---
+++ src/main/java/cat/itacademy/proyectoerp/ProyectoErpApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.proyectoerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/proyectoerp/ProyectoErpApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.proyectoerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
