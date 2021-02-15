19040ae67df54c478df893bebce83a38d9ff1a17
VictoriaParra <civediamo@hotmail.com.ar>
2021-02-15 09:40:29 +0100

    nueva version de IEmailService y EmailServiceImpl 2.0

--- src/main/java/cat/itacademy/proyectoerp/service/IEmailService.java
+++ src/main/java/cat/itacademy/proyectoerp/service/IEmailService.java
@@ -7,6 +7,6 @@ public interface IEmailService {
 	// method to send welcome email to new user
 	public void sendWelcomeEmail(User user);
 
-	// method to send the reset token to then be able to reset the password
-	public void passwordResetEmail(User user, String resetToken);
+	// method to send a new random password to user
+	public void sendPasswordEmail(User user, String password);
 }
