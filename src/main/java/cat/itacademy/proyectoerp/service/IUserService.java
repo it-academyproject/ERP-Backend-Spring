9007a7f893603e3894ee882b44a1b92d90f4aef6
VictoriaParra <civediamo@hotmail.com.ar>
2021-02-16 17:19:47 +0100

    new method added

--- src/main/java/cat/itacademy/proyectoerp/service/IUserService.java
+++ src/main/java/cat/itacademy/proyectoerp/service/IUserService.java
@@ -20,7 +20,7 @@ public interface IUserService {
 	public List<UserDTO> listAllClients();
 	public Optional<UserDTO> deleteUserById(Long id);
 	public Optional<UserDTO> modifyUser(Long id, User user);
- 
+	public User updatePassword(User user);
 
 
 
