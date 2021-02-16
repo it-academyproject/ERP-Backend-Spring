6d9352164ae4eae5d0dbab3f405edd59148f6b88
pierorepp90 <pierorepp90@gmail.com>
2021-02-16 10:48:46 +0100

    Order entity
    Order entity,repo,service and controller implementeds

--- src/main/java/cat/itacademy/proyectoerp/domain/Order.java
+++ src/main/java/cat/itacademy/proyectoerp/domain/Order.java
@@ -7,20 +7,17 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import cat.itacademy.proyectoerp.util.StringToListConverter;
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order{
 	
 	// Order entity attributes
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private UUID id;
 	@Column
 	private String employeeId;
