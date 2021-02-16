24cc37e352ca57ee7c440fc17d3eb96fd8cf264d
pierorepp90 <pierorepp90@gmail.com>
2021-02-16 08:23:14 +0100

    Update OrderServiceImpl.java

--- src/main/java/cat/itacademy/proyectoerp/service/OrderServiceImpl.java
+++ src/main/java/cat/itacademy/proyectoerp/service/OrderServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cat.itacademy.proyectoerp.dao.IOrderRepository;
+import cat.itacademy.proyectoerp.repository.IOrderRepository;
 import cat.itacademy.proyectoerp.domain.Order;
 
 @Service
