d8958fbda59bf3887f7262e240a25d3db3f98d35
VictoriaParra <civediamo@hotmail.com.ar>
2021-02-18 10:23:40 +0100

    Runner class version 1.1

--- src/main/java/cat/itacademy/proyectoerp/Runner.java
+++ src/main/java/cat/itacademy/proyectoerp/Runner.java
@@ -1,8 +1,9 @@
 package cat.itacademy.proyectoerp;
 
-import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import cat.itacademy.proyectoerp.domain.Client;
 import cat.itacademy.proyectoerp.domain.Order;
 import cat.itacademy.proyectoerp.domain.Product;
 import cat.itacademy.proyectoerp.domain.User;
+import cat.itacademy.proyectoerp.domain.UserType;
 import cat.itacademy.proyectoerp.service.ClientServiceImpl;
 import cat.itacademy.proyectoerp.service.OrderServiceImpl;
 import cat.itacademy.proyectoerp.service.ProductServiceImpl;
@@ -46,22 +48,14 @@ public class Runner implements CommandLineRunner {
 	@Override
 	@Transactional
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 
 		logger.info("initializing 2 users type client, 2 clients, 2 products and 2 orders");
 
-		// Initialize two products
-		Product productOne = new Product(1, "ejemplo 1", 100, "url image", "Bebidas", 3.00, 21.00, 2.50, 500);
-		productService.createProduct(productOne);
-
-		Product productTwo = new Product(2, "ejemplo 2", 100, "url image", "Bebidas", 3.00, 21.00, 2.50, 500);
-		productService.createProduct(productTwo);
-
 		// Initialize two Users type Client
-		User userClientOne = new User("userclientone@example.com", "ReW9a0&+TP");
+		User userClientOne = new User("userclientone@example.com", "ReW9a0&+TP", UserType.CLIENT);
 		userService.registerNewUserAccount(userClientOne);
 
-		User userClientTwo = new User("userclienttwo@example.com", "ReW9a0&+ET");
+		User userClientTwo = new User("userclienttwo@example.com", "ReW9a0&+ET", UserType.CLIENT);
 		userService.registerNewUserAccount(userClientTwo);
 
 		// Initialize two Clients
@@ -70,6 +64,25 @@ public class Runner implements CommandLineRunner {
 
 		Client clientTwo = new Client("address example", "L7654321Z", "url image", userClientTwo);
 		clientService.createClient(clientTwo);
+
+		// Initialize two products
+		Product productOne = new Product(1, "ejemplo 1", 100, "url image", "Bebidas", 3.00, 21.00, 2.50, 500);
+		productService.createProduct(productOne);
+
+		Product productTwo = new Product(2, "ejemplo 2", 100, "url image", "Bebidas", 3.00, 21.00, 2.50, 500);
+		productService.createProduct(productTwo);
+
+		// Initialize two orders
+
+		List<String> productsId = new ArrayList<String>(Arrays.asList("1", "2"));
+
+		Date today = new Date();
+
+		Order orderOne = new Order("1", clientOne.getid().toString(), today, "Completed", productsId);
+		orderService.createOrder(orderOne);
+
+		Order orderTwo = new Order("1", clientTwo.getid().toString(), today, "Completed", productsId);
+		orderService.createOrder(orderTwo);
 
 	}
 
