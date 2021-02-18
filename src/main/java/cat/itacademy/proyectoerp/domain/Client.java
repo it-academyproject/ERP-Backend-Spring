5bf6e966e702f8554deac1fa95108357a45242f7
SheikSama <jake.petrulla@gmail.com>
2021-02-18 12:35:03 +0100

    hotfix server

--- src/main/java/cat/itacademy/proyectoerp/domain/Client.java
+++ src/main/java/cat/itacademy/proyectoerp/domain/Client.java
@@ -4,6 +4,7 @@ package cat.itacademy.proyectoerp.domain;
 import java.util.UUID;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
