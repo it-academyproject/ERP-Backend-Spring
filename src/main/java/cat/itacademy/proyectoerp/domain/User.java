ebc1a900211465e60ac3494b11884b5a43bbacdb
Elena Piaggio <elenapiaggio@gmail.com>
2021-03-03 17:36:30 +0100

    UPDATE user class

--- src/main/java/cat/itacademy/proyectoerp/domain/User.java
+++ src/main/java/cat/itacademy/proyectoerp/domain/User.java
@@ -1,7 +1,6 @@
 package cat.itacademy.proyectoerp.domain;
 
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -19,114 +18,118 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 /**
- * User class. 
+ * User class.
  * <br>
  * Attributes: <br>
  * - Id : Auto <br>
  * - username : max length = 12 <br>
  * - password: max length = 16<br>
- * - userType: Enum type for role type.<br> 
- * 
- * @author IAcademy
+ * - userType: Enum type for role type.<br>
  *
+ * @author IAcademy
  */
-//@Inheritance(strategy = InheritanceType.JOINED)
+
+
 @Entity
-@Table(name="users")
-public class User   {
+@Table(name = "users")
+public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Email(message="email incorrect")
-    @Column(length=50)
-	@Size(min = 6, max = 50)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	private String username;
-																			
-    @Pattern(regexp="^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%?&+=]).*$",message="Password invalid. Minim 8 characters with 1 upercase, 1 lowercase, 1 special symbol, 1 number)")  
-	@Column(length=106)
-	private String password;
+  @Email(message = "email incorrect")
+  @Column(length = 50)
+  @Size(min = 6, max = 50)
 
-    @JsonProperty("user_type")
-	@Column(length=16)
-    @Enumerated(EnumType.STRING)
-	UserType userType; 
+  private String username;
 
-    @OneToOne(mappedBy = "user")
-    private Client client;
+  @Pattern(regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%?&+=]).*$", message = "Password invalid. Minim 8 characters with 1 upercase, 1 lowercase, 1 special symbol, 1 number)")
+  @Column(length = 106)
+  private String password;
 
-	
-	
-	public User() {
-		super();
-	}
+  @JsonProperty("user_type")
+  @Column(length = 16)
+  @Enumerated(EnumType.STRING)
+  UserType userType;
 
-	/**
-	 * Constructor. Create a User object with parameters. By default, userType is CLIENT. 	
-	 * @param username
-	 * @param password
-	 */
-	public User(@Size(min = 6, max = 50) String username, @Size(min = 8, max = 16) String password) {
-		super();
-		this.username = username;
-		this.password = password;
-		userType = UserType.CLIENT;
-	}
+  @OneToOne(mappedBy = "user")
+  private Client client;
+
+  @OneToOne(mappedBy = "user")
+  private Employee employee;
 
 
-	/**
-	 * Constructor. Create a User object with parameters.	
-	 * @param username
-	 * @param password
-	 * @param userType
-	 */
-	public User(String username, String password, UserType user_type) {
-		this.username = username;
-		this.password = password;
-		this.userType = user_type;
-	}
+  public User() {
+    super();
+  }
+
+  /**
+   * Constructor. Create a User object with parameters. By default, userType is CLIENT.
+   *
+   * @param username
+   * @param password
+   */
+  public User(@Size(min = 6, max = 50) String username, @Size(min = 8, max = 16) String password) {
+    super();
+    this.username = username;
+    this.password = password;
+    userType = UserType.CLIENT;
+  }
 
 
-	public String getUsername() {
-		return username;
-	}
+  /**
+   * Constructor. Create a User object with parameters.
+   *
+   * @param username
+   * @param password
+   * @param user_type
+   */
+  public User(String username, String password, UserType user_type) {
+    this.username = username;
+    this.password = password;
+    this.userType = user_type;
+  }
 
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public String getUsername() {
+    return username;
+  }
 
 
-	public String getPassword() {
-		return password;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
 
-	public UserType getUserType() {
-		return userType;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
 
-	public void setUserType(UserType user_type) {
-		this.userType = user_type;
-	}
+  public UserType getUserType() {
+    return userType;
+  }
 
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", userType=" + userType + "]";
-	}
+  public void setUserType(UserType user_type) {
+    this.userType = user_type;
+  }
 
 
-	public Long getId() {
-		return id;
-	}
+  @Override
+  public String toString() {
+    return "User [id=" + id + ", username=" + username + ", password=" + password + ", userType=" + userType + "]";
+  }
+
+
+  public Long getId() {
+    return id;
+  }
 
 }
