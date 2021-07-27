52fc4152ceb8fd68cf48d66866fb675939d9dd13
Cristina <crigraiba@gmail.com>
2021-07-27 12:50:56 +0200

    response status of custom exceptions in controller advice

--- src/main/java/cat/itacademy/proyectoerp/exceptions/ArgumentNotFoundException.java
+++ src/main/java/cat/itacademy/proyectoerp/exceptions/ArgumentNotFoundException.java
@@ -1,9 +1,5 @@
 package cat.itacademy.proyectoerp.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ArgumentNotFoundException extends RuntimeException {
 
 	/**
--- src/main/java/cat/itacademy/proyectoerp/exceptions/ArgumentNotValidException.java
+++ src/main/java/cat/itacademy/proyectoerp/exceptions/ArgumentNotValidException.java
@@ -1,9 +1,5 @@
 package cat.itacademy.proyectoerp.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ArgumentNotValidException extends RuntimeException {
 
 	/**
