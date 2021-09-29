214f25cf846360358942b7172cbfb918a4bec457
Lara Saiz <69240139+Larins@users.noreply.github.com>
2021-09-29 10:52:24 +0200

    B-117

--- src/main/java/cat/itacademy/proyectoerp/security/SecurityConfig.java
+++ src/main/java/cat/itacademy/proyectoerp/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests()						
 						.antMatchers(HttpMethod.GET, "/api/products").permitAll()
-						.antMatchers(HttpMethod.GET, "/api/products/{ID}").permitAll()				
+						.antMatchers(HttpMethod.GET, "/api/products/{ID}").permitAll()		
 						.antMatchers(HttpMethod.POST, "/api/users").permitAll()
 						.antMatchers("/api/login", "/api/users/clients", "/api/users/recoverpassword")
 						.permitAll().anyRequest().authenticated().and()
