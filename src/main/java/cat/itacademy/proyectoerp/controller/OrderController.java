8a2766f87b0a19f339575eca69b0381ab0d8946d
Daniel <danisolo91@gmail.com>
2021-10-04 21:37:16 +0200

    Change endpoint path

--- src/main/java/cat/itacademy/proyectoerp/controller/OrderController.java
+++ src/main/java/cat/itacademy/proyectoerp/controller/OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -117,15 +118,15 @@ public class OrderController {
 	
 	/**
 	 * 
-	 * Changes the status of an order by its ID. 
+	 * Updates the status of an order by its ID. 
 	 * 
 	 * @param orderId
 	 * @param orderStatusDTO
 	 * @return
 	 */
 	@PreAuthorize("hasRole('EMPLOYEE') or hasRole('ADMIN')")
-	@PostMapping("/orders/{orderId}/change-status")
-	public ResponseEntity<?> changeOrderStatus(@PathVariable UUID orderId, @RequestBody OrderStatusDTO orderStatusDTO) {
+	@PutMapping("/orders/{orderId}/status")
+	public ResponseEntity<?> updateOrderStatus(@PathVariable UUID orderId, @RequestBody OrderStatusDTO orderStatusDTO) {
 		MessageDTO messageDTO;
 		
 		try {
