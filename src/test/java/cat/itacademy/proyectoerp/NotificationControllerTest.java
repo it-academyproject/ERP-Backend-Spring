2891f41ba700af1e564ba29f53a2ade5b9ada6b3
Daniel <danisolo91@gmail.com>
2021-10-05 21:39:07 +0200

    Notification test for authenticated users

--- src/test/java/cat/itacademy/proyectoerp/NotificationControllerTest.java
+++ src/test/java/cat/itacademy/proyectoerp/NotificationControllerTest.java
@@ -1,15 +1,24 @@
 package cat.itacademy.proyectoerp;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cat.itacademy.proyectoerp.security.entity.JwtLogin;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,9 +30,34 @@ public class NotificationControllerTest {
 	
 	@Test
 	@DisplayName("Notifications for anonymous users are unauthorized")
-	void notificationsForAnonymousUsersReturns401() throws Exception {
+	void notificationsForAnonymousUsers() throws Exception {
 		mockMvc.perform(get("/api/notifications"))
 		.andExpect(status().isUnauthorized());
 	}
 	
+	@Test
+	@DisplayName("Authenticated users can get their notifications")
+	void notificationsForAuthenticatedUsers() throws Exception {
+		String accessToken = obtainAccessToken("client@erp.com", "ReW9a0&+TP");
+		
+		mockMvc.perform(get("/api/notifications")
+				.header("Authorization", "Bearer " + accessToken)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("object").isArray());
+	}
+	
+	private String obtainAccessToken(String username, String password) throws Exception {
+		JwtLogin jwtLogin = new JwtLogin(username, password);
+
+		ResultActions resultPost =
+				this.mockMvc.perform(post("/api/login")
+						.content(new ObjectMapper().writeValueAsString(jwtLogin))
+						.accept(MediaType.APPLICATION_JSON)
+						.contentType(MediaType.APPLICATION_JSON))
+						.andExpect(status().isOk());
+
+		String resultString = resultPost.andReturn().getResponse().getContentAsString();
+		return new JacksonJsonParser().parseMap(resultString).get("token").toString();
+	}
 }
