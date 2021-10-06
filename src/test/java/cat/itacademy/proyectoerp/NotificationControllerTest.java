5cb4b30247f5e3ef4a3b51f9b92f10a3cad5f06a
Daniel <danisolo91@gmail.com>
2021-10-06 12:57:44 +0200

    Improve test for authenticated users

--- src/test/java/cat/itacademy/proyectoerp/NotificationControllerTest.java
+++ src/test/java/cat/itacademy/proyectoerp/NotificationControllerTest.java
@@ -1,9 +1,12 @@
 package cat.itacademy.proyectoerp;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +21,9 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import cat.itacademy.proyectoerp.domain.OrderStatus;
 import cat.itacademy.proyectoerp.security.entity.JwtLogin;
+import cat.itacademy.proyectoerp.service.IOrderService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,6 +33,10 @@ public class NotificationControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private IOrderService orderService;
+	
+	// GET /api/notifications endpoint (as anonymous user)
 	@Test
 	@DisplayName("Notifications for anonymous users are unauthorized")
 	void notificationsForAnonymousUsers() throws Exception {
@@ -35,16 +44,20 @@ public class NotificationControllerTest {
 		.andExpect(status().isUnauthorized());
 	}
 	
+	// GET /api/notifications endpoint (as authenticated user)
 	@Test
-	@DisplayName("Authenticated users can get their notifications")
+	@DisplayName("Authenticated users can get their own notifications")
 	void notificationsForAuthenticatedUsers() throws Exception {
 		String accessToken = obtainAccessToken("client@erp.com", "ReW9a0&+TP");
+		
+		updateOrderStatus("11110000-0000-0000-0000-000000000000"); // generates a notification for the client
 		
 		mockMvc.perform(get("/api/notifications")
 				.header("Authorization", "Bearer " + accessToken)
 				.accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
-		.andExpect(jsonPath("object").isArray());
+		.andExpect(jsonPath("object").isArray())
+		.andExpect(jsonPath("object[0].user.username", is("client@erp.com")));
 	}
 	
 	private String obtainAccessToken(String username, String password) throws Exception {
@@ -59,5 +72,13 @@ public class NotificationControllerTest {
 
 		String resultString = resultPost.andReturn().getResponse().getContentAsString();
 		return new JacksonJsonParser().parseMap(resultString).get("token").toString();
+	}
+	
+	/**
+	 * Generates an ORDER_STATUS_CHANGED notification.
+	 */
+	private void updateOrderStatus(String id) {
+		UUID orderId = UUID.fromString(id);
+		orderService.updateOrderStatus(orderId, OrderStatus.IN_DELIVERY);
 	}
 }
